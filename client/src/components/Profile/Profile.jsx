af04dd33aefb5219614c055e30f60ebf16fc17a3
Christine Garaudy <christinegaraudy@gmail.com>
2020-10-05 13:55:00 -0500

    (add) makes app.jsx file

---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+// import { React } from 'react';
+// import ReactDOM from 'react-dom';
+
+// class Profile extends React (
+
+//   render() {
+//     return (
+//     <div>Welcome to React!</div>
+//     )
+  
+// )
+// }
+
+// export default Profile;
