51ba1e1994365af584b0d469da95920e31c367b2
Christine Garaudy <60481842+christinegaraudy@users.noreply.github.com>
2020-10-05 13:57:32 -0500

    Revert "(add) makes app.jsx file"

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => <div>Welcome to React!</div>;
+ReactDOM.render(<Index />, document.getElementById('root'));
