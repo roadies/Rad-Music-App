fb8137c0858839d8d15996f677d10b179fb20cca
lschwall <opsparklschwall@gmail.com>
2020-10-05 21:16:02 -0500

    (update) add.jsx

--- client/src/components/Add/Add.jsx
+++ client/src/components/Add/Add.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container, Row, Col, Nav,
+} from 'react-bootstrap';
 
 const Add = () => (
   <div as={Container} className="addPageView">
     <div as={Container} className="addPageContainer" style={{ border: 'solid 3px blue', padding: '5px', marginTop: '30px' }}>
       <Row>
-        <Col><div style={{ border: 'solid 10px red', padding: '100px', marginTop: '10px' }} /></Col>
+        <Col><div style={{ border: 'solid 4px red', padding: '100px' }} /></Col>
         <Col md="auto" />
-        <Col xs lg={2}>
-          <div className="add-nav" style={{ backgroundColor: '#C4C4C4', padding: '10px', marginTop: '30px' }}>
-            <Row>
-              Insert Add Page
-            </Row>
-            <Row>
-              Insert Search Page
-            </Row>
-            <Row>
-              Insert Profile Page
-            </Row>
+        <Col xs md={2}>
+          <div as={Container} className="add-nav" style={{ backgroundColor: '#C4C4C4', padding: '10px', display: 'flex' }}>
+            <Col>
+              <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="#">Add an Event</Nav.Link>
+                <Nav.Link eventKey="link-1">Search</Nav.Link>
+                <Nav.Link eventKey="link-2">Profile</Nav.Link>
+                <Nav.Link>Logout</Nav.Link>
+              </Nav>
+            </Col>
           </div>
         </Col>
       </Row>
