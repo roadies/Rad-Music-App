9b8438333c57382743da08da6e3386d4be4fdd4c
lschwall <opsparklschwall@gmail.com>
2020-10-05 19:50:13 -0500

    (add) layout for Add.jsx

--- client/src/components/Add/Add.jsx
+++ client/src/components/Add/Add.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Add = () => (
-  <div className="add-page-container">
-    <Container>
+  <div as={Container} className="addPageView">
+    <div as={Container} className="addPageContainer" style={{ border: 'solid 3px blue', padding: '5px', marginTop: '30px' }}>
       <Row>
-        <Row>
-          <Col xs={10} md={8}>1 of 3</Col>
-          <Col xs={2}>NavBar</Col>
-        </Row>
+        <Col><div style={{ border: 'solid 10px red', padding: '100px', marginTop: '10px' }} /></Col>
+        <Col md="auto" />
+        <Col xs lg={2}>
+          <div className="add-nav" style={{ backgroundColor: '#C4C4C4', padding: '10px', marginTop: '30px' }}>
+            <Row>
+              Insert Add Page
+            </Row>
+            <Row>
+              Insert Search Page
+            </Row>
+            <Row>
+              Insert Profile Page
+            </Row>
+          </div>
+        </Col>
       </Row>
-    </Container>
+    </div>
   </div>
 );
 
