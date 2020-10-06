608d821453f673ba7e16e65aa1faa4d3c0740241
Christine Garaudy <christinegaraudy@gmail.com>
2020-10-05 20:54:47 -0500

    (add) updates buttons and textboxes to be responsive

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import profilepic from './sample_data/profilepic.jpg';
 import Photos from './Photos';
 
@@ -21,12 +23,24 @@ class Profile extends Component {
           <Col sm={3}>
             <img src={profilepic} alt="cat in vest" style={{ height: 250, width: 250 }} />
             <div className="bio"> My name is Mittens.  I am 6 years old.  I love punk rock. </div>
-            <input type="text" style={{ height: 50, width: 250 }} />
-            <button className="bio_button" type="submit"> Update Bio </button>
+            <Form>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+            </Form>
+            <Button variant="outline-success" size="sm" block>
+              Update Bio
+            </Button>
           </Col>
           <Col md={6}>
-            <input type="text" style={{ width: '100%' }} />
-            <button className="status_button" type="submit"> Update Status </button>
+            <Form>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+            </Form>
+            <Button variant="outline-success" size="sm" block>
+              Update Status
+            </Button>
             <Photos />
           </Col>
           <Col sm={3}>
