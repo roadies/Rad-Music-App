526b3c22df85ffe48e778c51313d3ad4e1dfa024
mconner89 <john.conner@eagles.usm.edu>
2020-10-06 10:30:01 -0500

    (add) back-end

---
+++ server/api/shows.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const Shows = Router();
+
+Shows.get('/', (req, res) => {
+
+});
+
+Shows.post('/', (req, res) => {
+
+});
+
+module.exports = {
+  Shows,
+};
