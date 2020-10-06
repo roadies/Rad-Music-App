43a57ea83b2cc60ddda493b15a9c3bfa0ecbeb61
lschwall <opsparklschwall@gmail.com>
2020-10-06 18:50:32 -0500

    (add) Landing.jsx

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Landing = () => (
-  <div className="landing-page-containers">
-    Testing Landing Page
+  <div className="container-fluid" style={{ margin: 'auto' }}>
+    <Container>
+      <Row style={{ alignSelf: 'center' }}>
+        <div style={{ padding: '5px' }}>
+          These *genre* shows were found nearby!
+        </div>
+      </Row>
+      <Row style={{ marginRight: '0px', marginLeft: '0px' }}>
+        <Col style={{ padding: '5px' }} lg={10}>
+          <div style={{ display: 'block', height: '90vh', width: '50vw' }}>
+            <iframe
+              style={{ height: '90vh', width: '60vw' }}
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC5Ik6djb4DtWRTRdQGyBCYWnk0gtXWL_w
+    &q=NewOrleans+LA"
+              allowFullScreen
+              title="map"
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
