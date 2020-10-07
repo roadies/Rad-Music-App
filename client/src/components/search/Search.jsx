c298df94bcdbfec0fe5abfc817f3516180712ee6
lschwall <opsparklschwall@gmail.com>
2020-10-06 20:11:27 -0500

    (update) all files with main

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import {
+  Col, Container, Form, Row,
+} from 'react-bootstrap';
 
 const Search = () => {
   const [band, setBand] = useState('');
@@ -11,84 +10,166 @@ const Search = () => {
   const [genre, setGenre] = useState('');
 
   return (
-    <div as={Container} className="searchView">
-      <Row>
-        <Col>
-          <div as={Container} className="mapAndList">
-            <Row>
-              <Col>
-                <div className="searchList">
-                  list of shows
-                </div>
-              </Col>
-              <Col>
-                <div className="mapView">
-                  map
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col>
-          <div className="searchCriteria">
-            search criteria
-            <Form>
-              <Form.Group as={Row} controlId="formBand">
-                <Form.Label column sm={2}>
-                  Band:
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="text"
-                    placeholder="band name"
-                    value={band}
-                    onChange={(e) => setBand(e.target.value)}
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} controlId="formDate">
-                <Form.Label column sm={2}>
-                  Date:
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} controlId="formVenue">
-                <Form.Label column sm={2}>
-                  Venue:
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="text"
-                    placeholder="Venue name"
-                    value={venue}
-                    onChange={(e) => setVenue(e.target.value)}
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} controlId="formGenre">
-                <Form.Label column sm={2}>
-                  Genre:
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="text"
-                    placeholder="Genre name"
-                    value={genre}
-                    onChange={(e) => setGenre(e.target.value)}
-                  />
-                </Col>
-              </Form.Group>
-            </Form>
-          </div>
-        </Col>
-      </Row>
+    <div className="container-fluid" style={{ margin: 'auto' }}>
+      <Container>
+        <Row style={{ marginRight: '0px', marginLeft: '0px' }}>
+          <Col style={{ }} sm="auto">
+            <div className="add-page-form">
+              list of shows
+            </div>
+          </Col>
+          <Col style={{ padding: '5px' }} lg="auto">
+            <div style={{ display: 'block', height: '90vh', width: '40vw' }}>
+              <iframe
+                style={{ height: '90vh', width: '40vw' }}
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC5Ik6djb4DtWRTRdQGyBCYWnk0gtXWL_w
+    &q=NewOrleans+LA"
+                allowFullScreen
+                title="map"
+              />
+            </div>
+          </Col>
+          <Col lg="auto" style={{ }}>
+            <div>
+              <Form>
+                <Form.Group as={Row} controlId="formBand">
+                  <Form.Label column sm={2}>
+                    Band:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="text"
+                      placeholder="band name"
+                      value={band}
+                      onChange={(e) => setBand(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formDate">
+                  <Form.Label column sm={2}>
+                    Date:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="date"
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formVenue">
+                  <Form.Label column sm={2}>
+                    Venue:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Venue name"
+                      value={venue}
+                      onChange={(e) => setVenue(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formGenre">
+                  <Form.Label column sm={2}>
+                    Genre:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Genre name"
+                      value={genre}
+                      onChange={(e) => setGenre(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
+
+  // return (
+  //   <div as={Container} className="searchView">
+  //     <Row>
+  //       <Col>
+  //         <div as={Container} className="mapAndList">
+  //           <Row>
+  //             <Col>
+  //               <div className="searchList">
+  //                 list of shows
+  //               </div>
+  //             </Col>
+  //             <Col>
+  //               <div className="mapView">
+  //                 map
+  //               </div>
+  //             </Col>
+  //           </Row>
+  //         </div>
+  //       </Col>
+  //       <Col>
+  //         <div className="searchCriteria">
+  //           search criteria
+  //           <Form>
+  //             <Form.Group as={Row} controlId="formBand">
+  //               <Form.Label column sm={2}>
+  //                 Band:
+  //               </Form.Label>
+  //               <Col sm={10}>
+  //                 <Form.Control
+  //                   type="text"
+  //                   placeholder="band name"
+  //                   value={band}
+  //                   onChange={(e) => setBand(e.target.value)}
+  //                 />
+  //               </Col>
+  //             </Form.Group>
+  //             <Form.Group as={Row} controlId="formDate">
+  //               <Form.Label column sm={2}>
+  //                 Date:
+  //               </Form.Label>
+  //               <Col sm={10}>
+  //                 <Form.Control
+  //                   type="date"
+  //                   value={date}
+  //                   onChange={(e) => setDate(e.target.value)}
+  //                 />
+  //               </Col>
+  //             </Form.Group>
+  //             <Form.Group as={Row} controlId="formVenue">
+  //               <Form.Label column sm={2}>
+  //                 Venue:
+  //               </Form.Label>
+  //               <Col sm={10}>
+  //                 <Form.Control
+  //                   type="text"
+  //                   placeholder="Venue name"
+  //                   value={venue}
+  //                   onChange={(e) => setVenue(e.target.value)}
+  //                 />
+  //               </Col>
+  //             </Form.Group>
+  //             <Form.Group as={Row} controlId="formGenre">
+  //               <Form.Label column sm={2}>
+  //                 Genre:
+  //               </Form.Label>
+  //               <Col sm={10}>
+  //                 <Form.Control
+  //                   type="text"
+  //                   placeholder="Genre name"
+  //                   value={genre}
+  //                   onChange={(e) => setGenre(e.target.value)}
+  //                 />
+  //               </Col>
+  //             </Form.Group>
+  //           </Form>
+  //         </div>
+  //       </Col>
+  //     </Row>
+  //   </div>
+  // );
 };
 export default Search;
