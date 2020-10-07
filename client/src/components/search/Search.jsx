f908870bff8fe9f361057eab400aab8f3844883b
lschwall <opsparklschwall@gmail.com>
2020-10-07 11:21:30 -0500

    (update) search formatting

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -13,77 +13,67 @@ const Search = () => {
     <div className="container-fluid" style={{ margin: 'auto' }}>
       <Container>
         <Row style={{ marginRight: '0px', marginLeft: '0px' }}>
-          <Col style={{ }} sm="auto">
-            <div className="add-page-form">
-              list of shows
-            </div>
+          <Col lg={2}>
+            <Row style={{ padding: '10px' }}>
+              <div className="add-page-form">
+                <Form>
+                  <Form.Group as={Row} controlId="formBand">
+                    <Col sm={11}>
+                      <Form.Control
+                        type="text"
+                        placeholder="band name"
+                        value={band}
+                        onChange={(e) => setBand(e.target.value)}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="formDate">
+                    <Col sm={11}>
+                      <Form.Control
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="formVenue">
+                    <Col sm={11}>
+                      <Form.Control
+                        type="text"
+                        placeholder="Venue name"
+                        value={venue}
+                        onChange={(e) => setVenue(e.target.value)}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="formGenre">
+                    <Col sm={11}>
+                      <Form.Control
+                        type="text"
+                        placeholder="Genre name"
+                        value={genre}
+                        onChange={(e) => setGenre(e.target.value)}
+                      />
+                    </Col>
+                  </Form.Group>
+                </Form>
+              </div>
+            </Row>
+            <Row>
+              <div style={{ border: 'solid 1px green', padding: '10px' }}>
+                List of Shows goes here
+              </div>
+            </Row>
           </Col>
-          <Col style={{ padding: '5px' }} lg="auto">
-            <div style={{ display: 'block', height: '90vh', width: '40vw' }}>
+          <Col style={{ padding: '5px' }} lg={10}>
+            <div style={{ display: 'block', height: '90vh', width: '50vw' }}>
               <iframe
-                style={{ height: '90vh', width: '40vw' }}
+                style={{ height: '90vh', width: '60vw' }}
                 src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC5Ik6djb4DtWRTRdQGyBCYWnk0gtXWL_w
     &q=NewOrleans+LA"
                 allowFullScreen
                 title="map"
               />
-            </div>
-          </Col>
-          <Col lg="auto" style={{ }}>
-            <div>
-              <Form>
-                <Form.Group as={Row} controlId="formBand">
-                  <Form.Label column sm={2}>
-                    Band:
-                  </Form.Label>
-                  <Col sm={10}>
-                    <Form.Control
-                      type="text"
-                      placeholder="band name"
-                      value={band}
-                      onChange={(e) => setBand(e.target.value)}
-                    />
-                  </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formDate">
-                  <Form.Label column sm={2}>
-                    Date:
-                  </Form.Label>
-                  <Col sm={10}>
-                    <Form.Control
-                      type="date"
-                      value={date}
-                      onChange={(e) => setDate(e.target.value)}
-                    />
-                  </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formVenue">
-                  <Form.Label column sm={2}>
-                    Venue:
-                  </Form.Label>
-                  <Col sm={10}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Venue name"
-                      value={venue}
-                      onChange={(e) => setVenue(e.target.value)}
-                    />
-                  </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formGenre">
-                  <Form.Label column sm={2}>
-                    Genre:
-                  </Form.Label>
-                  <Col sm={10}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Genre name"
-                      value={genre}
-                      onChange={(e) => setGenre(e.target.value)}
-                    />
-                  </Col>
-                </Form.Group>
-              </Form>
             </div>
           </Col>
         </Row>
