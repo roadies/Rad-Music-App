c1422f08f5699aed01733d3a947dbc864a35a5c5
lschwall <opsparklschwall@gmail.com>
2020-10-07 17:07:26 -0500

    (update) re deleting keys

--- client/src/components/Add/Add.jsx
+++ client/src/components/Add/Add.jsx
@@ -39,7 +39,7 @@ const Add = () => (
           <div style={{ display: 'block', height: '90vh', width: '50vw' }}>
             <iframe
               style={{ height: '90vh', width: '60vw' }}
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC5Ik6djb4DtWRTRdQGyBCYWnk0gtXWL_w
+              src="https://www.google.com/maps/embed/v1/place?key=
     &q=NewOrleans+LA"
               allowFullScreen
               title="map"
--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -14,7 +14,7 @@ const Landing = () => (
           <div style={{ display: 'block', height: '90vh', width: '50vw' }}>
             <iframe
               style={{ height: '90vh', width: '60vw' }}
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC5Ik6djb4DtWRTRdQGyBCYWnk0gtXWL_w
+              src="https://www.google.com/maps/embed/v1/place?key=
     &q=NewOrleans+LA"
               allowFullScreen
               title="map"
