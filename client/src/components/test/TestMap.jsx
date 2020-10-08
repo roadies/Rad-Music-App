e874a28bfeaf6740de6270d4fcb835d30d2533d6
lschwall <opsparklschwall@gmail.com>
2020-10-08 16:50:33 -0500

    (PROTECT) api key

--- client/src/components/test/TestMap.jsx
+++ client/src/components/test/TestMap.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
+import React, { useState, useCallback, useRef } from 'react';
 import {
   GoogleMap,
   useLoadScript,
@@ -6,11 +10,12 @@ import {
   InfoWindow,
 } from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
+import SearchLocation from './SearchLocation';
 import mapStyles from './styles';
 
 const libraries = ['places'];
 const mapContainerStyle = {
-  width: '80vw',
+  width: '100vw',
   height: '100vh',
 };
 const center = {
@@ -29,20 +34,70 @@ export default function Map() {
     libraries,
   });
 
-  if (loadError) {
-    return 'ERROR LOADING MAPS';
-  }
-  if (!isLoaded) {
-    return 'LOADING MAPS';
-  }
+  const [markers, setMarkers] = useState([]);
+  const [selectMarker, setSelectedMarker] = useState(null);
+
+  const onMapClick = useCallback((event) => {
+    setMarkers((current) => [...current, {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+      time: new Date(),
+    }]);
+    console.log('lat', event.latLng.lat());
+    console.log('lng', event.latLng.lng());
+  }, []);
+
+  const mapReference = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapReference.current = map;
+  }, []);
+
+  if (loadError) return 'ERROR LOADING MAPS';
+  if (!isLoaded) return 'LOADING MAPS';
+
   return (
     <div>
+
+      <SearchLocation />
+
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={8}
+        zoom={12}
         center={center}
         options={options}
-      />
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: 'https://i.imgur.com/zSMeNvZ.png',
+              scaledSize: new window.google.maps.Size(40, 40),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(20, 20),
+            }}
+            onClick={() => {
+              setSelectedMarker(marker);
+            }}
+          />
+        ))}
+        {selectMarker ? (
+          <InfoWindow position={{ lat: selectMarker.lat, lng: selectMarker.lng }} onCloseClick={() => { setSelectedMarker(null); }}>
+            <div>
+              <h1>
+                Show added
+              </h1>
+              <p>
+                Time of Show:
+                {' '}
+                {formatRelative(selectMarker.time, new Date())}
+              </p>
+            </div>
+          </InfoWindow>
+        ) : null }
+      </GoogleMap>
     </div>
   );
 }
