5da487fd8f1083865e61480f175efa806d7042f8
lschwall <opsparklschwall@gmail.com>
2020-10-10 15:17:24 -0500

    (add) gallery.jsx

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import {} from 'react-bootstrap';
+import GalleryList from './GalleryList';
+import ImageLoader from './ImageLoader';
 
-const Gallery = () => {
-  const test = {};
-
-  return (
-    <div>
-      testing gallery
+const Gallery = () => (
+  <div>
+    <div className="Gallery-Image-Upload">
+      <ImageLoader />
     </div>
+  </div>
 
-  );
-};
+);
 
 export default Gallery;
