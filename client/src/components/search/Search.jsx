b0feb3db3c037183429362f974cb75064e6244ce
lschwall <opsparklschwall@gmail.com>
2020-10-10 10:48:59 -0500

    (add) gallery.jsx

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,111 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import Axios from 'axios';
-import {
-  Col, Container, Form, Row, Button,
-} from 'react-bootstrap';
-
-const Search = () => {
-  const [band, setBand] = useState('Nickelback');
-  const [date, setDate] = useState('2020-10-10');
-  const [venue, setVenue] = useState('The Willow, Willow Street, New Orleans, LA, USA ');
-  const [genre, setGenre] = useState('Alternative');
-
-  const getShows = (query, type) => {
-    const params = { query, type };
-    if (type === 'band') {
-      Axios.get('/api/shows/band', { params })
-        .then((response) => {
-          console.log(response);
-        });
-    } else if (type === 'venue') {
-      Axios.get('/api/shows/venue', { params })
-        .then((response) => {
-          console.log(response);
-        });
-    } else if (type === 'genre') {
-      // TODO: add dis
-    } else if (type === 'date') {
-      // TODO: add dis
-    }
-  };
-
-  return (
-    <div className="container-fluid" style={{ margin: 'auto' }}>
-      <Container>
-        <Row style={{ marginRight: '0px', marginLeft: '0px' }}>
-          <Col lg={2}>
-            <Row style={{ padding: '10px' }}>
-              <div className="add-page-form">
-                <Form>
-                  <Form.Group as={Row} controlId="formBand">
-                    <Col sm={11}>
-                      <Form.Control
-                        type="text"
-                        placeholder="band name"
-                        value={band}
-                        onChange={(e) => setBand(e.target.value)}
-                      />
-                    </Col>
-                  </Form.Group>
-                  <Form.Group as={Row} controlId="formDate">
-                    <Col sm={11}>
-                      <Form.Control
-                        type="date"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                      />
-                    </Col>
-                  </Form.Group>
-                  <Form.Group as={Row} controlId="formVenue">
-                    <Col sm={11}>
-                      <Form.Control
-                        type="text"
-                        placeholder="Venue name"
-                        value={venue}
-                        onChange={(e) => setVenue(e.target.value)}
-                      />
-                    </Col>
-                  </Form.Group>
-                  <Form.Group as={Row} controlId="formGenre">
-                    <Col sm={11}>
-                      <Form.Control
-                        type="text"
-                        placeholder="Genre name"
-                        value={genre}
-                        onChange={(e) => setGenre(e.target.value)}
-                      />
-                    </Col>
-                  </Form.Group>
-                  <Button
-                    as={Row}
-                    type="reset"
-                    onClick={() => getShows(venue, 'venue')}
-                  >
-                    Submit
-                  </Button>
-                </Form>
-              </div>
-            </Row>
-            <Row>
-              <div style={{ border: 'solid 1px green', padding: '10px' }}>
-                List of Shows goes here
-              </div>
-            </Row>
-          </Col>
-          <Col style={{ padding: '5px' }} lg={10}>
-            <div style={{ display: 'block', height: '90vh', width: '50vw' }}>
-              <iframe
-                style={{ height: '90vh', width: '40vw' }}
-                src=""
-                allowFullScreen
-                title="map"
-              />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-=======
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
@@ -118,7 +10,6 @@ import {
   InfoWindow,
 } from '@react-google-maps/api';
 import Axios from 'axios';
-import { Form, Col, Button } from 'react-bootstrap';
 import mapStyles from '../Add/styles';
 
 const libraries = ['places'];
@@ -153,31 +44,25 @@ const Search = ({ user, genre }) => {
   return (
     <div>
       <div style={{ border: 'solid green 1px', padding: '10px' }}>
-        <Form>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Band Name</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Group controlId="formDate">
-            <Form.Label>Date</Form.Label>
-            <Form.Control
-              type="date"
-              placeholder="mm/dd/yyy"
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formGridAddress2">
-            <Form.Label>Venue</Form.Label>
-            <Form.Control placeholder="Apartment, studio, or floor" />
-          </Form.Group>
-
-          <Button variant="primary">
-            Search
-          </Button>
-        </Form>
+        <div>
+          <h5>
+            Welcome to your radically awesome music assistant,
+          </h5>
+          <h4>
+            {' '}
+            {user}
+            !
+          </h4>
+        </div>
+        <div>
+          Please have a look at the map below to see
+          <h6>
+            {' '}
+            {genre}
+            {' '}
+          </h6>
+          shows nearby!
+        </div>
       </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
@@ -196,7 +81,6 @@ const Search = ({ user, genre }) => {
           }}
         />
       </GoogleMap>
->>>>>>> 288715a... (update) Search view with new map and form
     </div>
   );
 };
