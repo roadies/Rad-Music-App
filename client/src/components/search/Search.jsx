519b2ae7014295015840c9d09a1c0b99e7790978
lschwall <opsparklschwall@gmail.com>
2020-10-11 01:03:20 -0500

    (update) ZOOM OUT YOU IDIOT

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -11,6 +11,8 @@ import {
 } from '@react-google-maps/api';
 import Axios from 'axios';
 import { Form, Col, Button } from 'react-bootstrap';
+import { formatRelative } from 'date-fns';
+import { set } from 'js-cookie';
 import SearchTab from './SearchTab';
 import mapStyles from '../Add/styles';
 
@@ -30,6 +32,20 @@ const options = {
 };
 
 const Search = ({ user, genre }) => {
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  // const test = [];
+
+  // const onMapClick = useCallback((e) => {
+  //   setMarkers((e) => {
+  //     test.forEach(coordSets => {
+
+  //     })
+  //   });
+  //   console.log(markers, '<---');
+  // }, []);
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
@@ -39,22 +55,33 @@ const Search = ({ user, genre }) => {
   const onMapLoad = useCallback((map) => {
     mapReference.current = map;
   }, []);
+  const test = [];
+
   const getShows = (query, type) => {
     const params = { query, type };
     if (type === 'band') {
       Axios.get('/api/shows/band', { params })
-        .then((response) => {
-          console.log(response);
+        .then(({ data }) => {
+          data.forEach((coords) => {
+            test.push({ lat: Number(coords.lat), lng: Number(coords.lng) });
+          });
+          setMarkers(test);
         });
     } else if (type === 'venue') {
       Axios.get('/api/shows/venue', { params })
-        .then((response) => {
-          console.log(response);
+        .then(({ data }) => {
+          data.forEach((coords) => {
+            test.push({ lat: Number(coords.lat), lng: Number(coords.lng) });
+          });
+          setMarkers(test);
         });
     } else if (type === 'date') {
       Axios.get('/api/shows/date', { params })
-        .then((response) => {
-          console.log(response);
+        .then(({ data }) => {
+          data.forEach((coords) => {
+            test.push({ lat: Number(coords.lat), lng: Number(coords.lng) });
+          });
+          setMarkers(test);
         });
     }
   };
@@ -65,24 +92,58 @@ const Search = ({ user, genre }) => {
     <div>
       <div style={{ border: 'solid green 1px', padding: '10px' }}>
         {/* add in views here with tabs */}
-        <SearchTab />
+        <SearchTab getShows={getShows} />
       </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={12}
         center={center}
         options={options}
+        // onClick={onMapClick}
         onLoad={onMapLoad}
       >
-        <Marker
-          position={{ lat: 30, lng: -90 }} // positions will change -> lat,lng address
-          icon={{
-            url: 'https://i.imgur.com/zSMeNvZ.png', // grabs an image from imgur and sets it as marker
-            scaledSize: new window.google.maps.Size(40, 40),
-            origin: new window.google.maps.Point(0, 0),
-            anchor: new window.google.maps.Point(20, 20),
-          }}
-        />
+
+        {markers.map((marker, id) => (
+          <Marker
+            key={id}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            //   // onClick={() => {
+            //   //   setSelected(marker);
+            //   // }}
+            // icon={{
+            //   url: 'https://i.imgur.com/xRAaYI3.jpg',
+            //   origin: new window.google.maps.Point(0, 0),
+            //   anchor: new window.google.maps.Point(15, 15),
+            //   scaledSize: new window.google.maps.Size(30, 30),
+            // }}
+          />
+        ))}
+        {/* <Marker
+          position={{ lat: 29.9495, lng: -90.1294 }}
+        /> */}
+        {/* {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                <span role="img" aria-label="bear">
+                  🐻
+                </span>
+                {' '}
+                Alert
+              </h2>
+              <p>
+                Spotted
+                {' '}
+                {formatRelative(selected.time, new Date())}
+              </p>
+            </div>
+          </InfoWindow>
+        ) : null} */}
       </GoogleMap>
     </div>
   );
