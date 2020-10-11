eb0b71d5a8609a92463da1d37e95e3ea164e54d0
lschwall <opsparklschwall@gmail.com>
2020-10-10 19:12:11 -0500

    (update) Search view with new map and form

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from 'react';
 import Axios from 'axios';
 import {
@@ -104,6 +105,91 @@ const Search = () => {
           </Col>
         </Row>
       </Container>
+=======
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
+import React, { useState, useCallback, useRef } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import Axios from 'axios';
+import mapStyles from '../Add/styles';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100%',
+  height: '80vh',
+};
+const center = {
+  lat: 30,
+  lng: -90,
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+const Search = ({ user, genre }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const mapReference = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapReference.current = map;
+  }, []);
+
+  if (loadError) return 'ERROR LOADING MAPS';
+  if (!isLoaded) return 'LOADING MAPS';
+
+  return (
+    <div>
+      <div style={{ border: 'solid green 1px', padding: '10px' }}>
+        <div>
+          <h5>
+            Welcome to your radically awesome music assistant,
+          </h5>
+          <h4>
+            {' '}
+            {user}
+            !
+          </h4>
+        </div>
+        <div>
+          Please have a look at the map below to see
+          <h6>
+            {' '}
+            {genre}
+            {' '}
+          </h6>
+          shows nearby!
+        </div>
+      </div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+        onLoad={onMapLoad}
+      >
+        <Marker
+          position={{ lat: 30, lng: -90 }} // positions will change -> lat,lng address
+          icon={{
+            url: 'https://i.imgur.com/zSMeNvZ.png', // grabs an image from imgur and sets it as marker
+            scaledSize: new window.google.maps.Size(40, 40),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(20, 20),
+          }}
+        />
+      </GoogleMap>
+>>>>>>> 288715a... (update) Search view with new map and form
     </div>
   );
 };
