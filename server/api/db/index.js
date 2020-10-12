a344876dd0cfa78450d0b80c000c266cd3703d34
mconner89 <john.conner@eagles.usm.edu>
2020-10-11 21:31:43 -0500

    (update) documentation

--- server/api/db/index.js
+++ server/api/db/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 // const { now } = require('sequelize/types/lib/utils');
 const { SEQUEL_PASS } = require('../config');
 
+// creates database. if dev environment, no password
 const db = process.env.ENVIRON === 'dev' ? new Sequelize('radma', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
@@ -13,6 +14,7 @@ const db = process.env.ENVIRON === 'dev' ? new Sequelize('radma', 'root', '', {
   logging: false,
 });
 
+// define show table
 const Show = db.define('shows', {
   id: {
     type: Sequelize.INTEGER,
@@ -26,6 +28,7 @@ const Show = db.define('shows', {
   details: Sequelize.STRING,
 });
 
+// define genre table
 const Genre = db.define('genres', {
   id: {
     type: Sequelize.INTEGER,
@@ -35,6 +38,7 @@ const Genre = db.define('genres', {
   genreName: Sequelize.STRING,
 });
 
+// define band table
 const Band = db.define('bands', {
   id: {
     type: Sequelize.INTEGER,
@@ -45,6 +49,7 @@ const Band = db.define('bands', {
   genreId: Sequelize.INTEGER,
 });
 
+// define user table
 const User = db.define('users', {
   id: {
     type: Sequelize.INTEGER,
@@ -59,6 +64,7 @@ const User = db.define('users', {
   profilePrompt: Sequelize.BOOLEAN,
 });
 
+// define shows/bands join table
 const ShowsBands = db.define('shows_bands', {
   id: {
     type: Sequelize.INTEGER,
@@ -69,22 +75,24 @@ const ShowsBands = db.define('shows_bands', {
   showId: Sequelize.INTEGER,
 });
 
-// adds genreId to band
+// links band and genre table by adding foreign key to band table
 Band.belongsTo(Genre);
 
-// add genreID to user
+// links user and genre table by adding foreign key to user table
 User.belongsTo(Genre);
 
-// create shows bands join table
+// define shows/bands join table
 Band.belongsToMany(Show, { through: ShowsBands });
 Show.belongsToMany(Band, { through: ShowsBands });
 
+// create tables if they don't exist, do nothing if they do exist
 Show.sync();
 Genre.sync();
 User.sync();
 Band.sync();
 ShowsBands.sync();
 
+// connect to database
 db.authenticate()
   .then(() => {
     console.log('connected to database');
@@ -93,6 +101,7 @@ db.authenticate()
     console.log(error, 'not connected to database');
   });
 
+// function that authenticates w/ google id
 const authFunc = (profile) => User.findOne({
   where: {
     googleId: profile.id,
--- server/api/profile.js
+++ server/api/profile.js
@@ -1,25 +1,20 @@
-const { useLoadScript } = require('@react-google-maps/api');
 const { Router } = require('express');
 const { User, Genre } = require('../db/index');
 
 const Profile = Router();
 
-Profile.get('/', (req, res) => {
-  res.send('idk man');
-});
-
-Profile.post('/', (req, res) => {
-
-});
-
 // handles requests from new user  creation page and creates new user
 Profile.post('/create', (req, res) => {
   const { userName, setupGenre, user } = req.body;
+  // search for user matching username from client
   User.findOne({ where: { userName } })
     .then((response) => {
+      // if username doesnt exist
       if (!response) {
+        // find genreId for selected genre
         Genre.findOne({ where: { genreName: setupGenre }, attributes: ['id'] })
           .then((result) => {
+            // replace temporary username with selected username and relevant info
             User.update({
               userName,
               profilePrompt: true,
@@ -34,8 +29,8 @@ Profile.post('/create', (req, res) => {
         throw user;
       }
     })
+    // if username has already been used, send failure message
     .catch((error) => {
-      console.log(error, 'error');
       res.status(400).send('failure');
     });
 });
