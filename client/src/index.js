38954e2410845522274e9c8f7edb0f318c17ec5f
lschwall <opsparklschwall@gmail.com>
2020-10-11 19:44:28 -0500

    test

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/REAL APP/App';
+import App from './components/App';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
