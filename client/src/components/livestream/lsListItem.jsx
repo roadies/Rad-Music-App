824b2bd67be61e28b2c1c43b76d14ddb55be3347
Christopher Booth <christopherbooth@ChristophersMBP.attlocal.net>
2020-10-14 18:59:12 -0500

    (add) livestreams now render to page and play upon click!

--- client/src/components/livestream/lsListItem.jsx
+++ client/src/components/livestream/lsListItem.jsx
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import SingleLivestream from './singleLivestream.jsx';
 import {} from 'react-bootstrap';
 
 const LsListItem = (props) => {
     const { stream } = props;
-    const [livestreamObj, setLivestreamObj] = useState();
+    const [isClicked, toggleClicked] = useState(false);
+    // const [livestreamObj, setLivestreamObj] = useState();
 
+    const handleClick = () => {
+        isClicked ? toggleClicked(false) : toggleClicked(true);
+        console.log(isClicked, 'Clicked!');
+    }
 
     useEffect( () => {
         console.log(props);
     }, [props.stream]);
     // if (!livestreams) {
-    return (
-        <div>
-            <h1>{stream.publisher.stream}</h1>
-        </div>
+    
+    if (isClicked){ 
+        return (
+            <div onClick={handleClick}>
+                <SingleLivestream stream={stream} />
+            </div>
+        )
+        } else {
+            return (
 
-    );
+            <div onClick={handleClick}>
+                <h1>{stream.publisher.stream}</h1>
+                <h3>isClicked is false!</h3>
+            </div>
+            )
+        }
 
 };
 
---
+++ client/src/components/livestream/singleLivestream.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import {} from 'react-bootstrap';
+
+
+const SingleLivestream = (props) => {
+    const { stream } = props;
+    useEffect( () => {
+        const createPlayer = () => {
+            if (flvjs.isSupported()) {
+                const videoElement = document.getElementById(`${stream.publisher.clientId}`);
+                const flvPlayer = flvjs.createPlayer({
+                    type: 'flv',
+                    url: `http://localhost:8000/live/${stream.publisher.stream}.flv`
+                });
+                flvPlayer.attachMediaElement(videoElement);
+                flvPlayer.load();
+                flvPlayer.play();
+            }
+        }
+
+        createPlayer()
+    })
+    return (
+        <video id={stream.publisher.clientId} controls height="400" width="600"></video>
+    );
+};
+
+export default SingleLivestream;
